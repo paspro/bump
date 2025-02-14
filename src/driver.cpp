eefc3707dc0a2ca2588d7a75b07faabf03d57b9b
Panos Asproulis <panos.asproulis@renuda.com>
2025-02-14 15:56:12 +0100

    Format changes

--- src/driver.cpp
+++ src/driver.cpp
@@ -31,31 +31,25 @@ int main()
     length  = 25.0; // length of the domain (m)
     n_cells = 200;  // number of cells for the computation
     Bump::initialise_geometry(length, n_cells);
-
     //
-    // Subcritical flow over a bump
+    // Subcritical flow over a bump.
     //
     q_in     = 4.42;                   // water flow rate at x = 0 (m^2/s)
     h_out    = 2.00;                   // water height at the outlet (m)
     filename = "subcritical_bump.dat"; // name of the file to save the results
-
     Bump::compute_subcritical_case(q_in, h_out, 9.81, filename, true);
-
     //
-    // Transcritical no-shock flow over a bump
+    // Transcritical no-shock flow over a bump.
     //
     q_in     = 1.53;                              // water flow rate at x = 0 (m^2/s)
-    filename = "transcritical_no_shock_bump.dat"; // name of the file to save
-                                                  // the results
+    filename = "transcritical_no_shock_bump.dat"; // name of the file to save the results
     Bump::compute_transcritical_no_shock_case(q_in, 9.81, filename, true);
-
     //
-    // Transcritical shock flow over a bump
+    // Transcritical shock flow over a bump.
     //
     q_in     = 0.18;                           // water flow rate at x = 0 (m^2/s)
     h_out    = 0.33;                           // water height at the outlet (m)
-    filename = "transcritical_shock_bump.dat"; // name of the file to save
-                                               // the results
+    filename = "transcritical_shock_bump.dat"; // name of the file to save the results
     Bump::compute_transcritical_shock_case(q_in, h_out, 9.81, filename, true);
 
     return EXIT_SUCCESS;
